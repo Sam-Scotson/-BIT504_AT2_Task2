1e4a894d2bc0d0432812261673026426851366d0
Sam Scotson <118854225+Sam-Scotson@users.noreply.github.com>
2024-06-06 21:34:47 +1200

    Add files via upload

--- Pong/src/pong/PongPanel.java
+++ Pong/src/pong/PongPanel.java
@@ -17,6 +17,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
     private static final int DELAY = 10; // Milliseconds between timer ticks
     private static final Color BACKGROUND_COLOUR = Color.BLACK;
     private final static int TIMER_DELAY = 5;
+    private static final int BALL_MOVEMENT_SPEED = 5;
     GameState gameState = GameState.INITIALISING;
     Ball ball;
     Paddle paddle1, paddle2;
@@ -36,31 +37,36 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
     }
 
     private void update() {
-           switch(gameState) {
-               case INITIALISING: {
-                   createObjects();
-                  gameState = GameState.PLAYING;
-                   break;
-               }
-               case PLAYING: {
-                   break;
-             }
-             case GAMEOVER: {
-                 break;
-             }
-         }
-     }
+        switch(gameState) {
+            case INITIALISING: {
+                createObjects();
+                ball.setXVelocity(BALL_MOVEMENT_SPEED);
+                ball.setYVelocity(BALL_MOVEMENT_SPEED);
+                gameState = GameState.PLAYING;
+                break;
+            }
+            case PLAYING: {
+            	moveObject(ball);
+            	checkWallBounce();
+                break;
+            }
+            case GAMEOVER: {
+                break;
+            }
+        }
+    }
 
     @Override
-      public void paintComponent(Graphics g) {
-          super.paintComponent(g);
-          paintDottedLine(g);
-          if(gameState != GameState.INITIALISING) {
-              paintSprite(g, ball);
-              paintSprite(g, paddle1);
-              paintSprite(g, paddle2);
-          }
-      }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        paintDottedLine(g);
+        if(gameState!= GameState.INITIALISING) {
+            paintSprite(g, ball);
+            paintSprite(g, paddle1);
+            paintSprite(g, paddle2);
+        }
+    }
+
     private void paintDottedLine(Graphics g) {
         Graphics2D g2d = (Graphics2D) g.create();
         Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
@@ -74,6 +80,28 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
         g.setColor(sprite.getColour());
         g.fillRect(sprite.getXPosition(), sprite.getYPosition(), sprite.getWidth(), sprite.getHeight());
     }
+
+    private void moveObject(Sprite sprite) {
+        int newX = sprite.getXPosition() + sprite.getXVelocity();
+        int newY = sprite.getYPosition() + sprite.getYVelocity();
+        sprite.setXPosition(newX, getWidth());
+        sprite.setYPosition(newY, getHeight());
+    }
+    
+    private void resetBall() {
+    	ball.resetToInitialPosition();
+    }
+    
+    private void checkWallBounce() {
+    	if (ball.getXPosition() <= 0 || ball.getXPosition() + ball.getWidth() >= getWidth()) {
+    		resetBall();
+    	}
+    	if (ball.getYPosition() <= 0 || ball.getYPosition() + ball.getHeight() >= getHeight()) {
+    		ball.setYVelocity(-ball.getYVelocity());
+    	}
+    }
+    
+
 
     @Override
     public void keyTyped(KeyEvent event) {
